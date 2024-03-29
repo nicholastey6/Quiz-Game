5488cef6e598ea6c534d5b137f8bf1c2f7e0106c
nicholastey6 <163625427+nicholastey6@users.noreply.github.com>
2024-03-29 20:10:49 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for i in range(len(question_data)):
+    question_bank.append(Question(question_data[i]["text"],question_data[i]["answer"]))
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz, your final score is {quiz.score}/{quiz.question_number}")
